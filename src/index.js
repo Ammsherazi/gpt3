7ff4999991bd812f2d85bf658076c654ee9c3afc
ammsherazi <syedammarhaider14@gmail.com>
2023-09-21 00:02:30 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
+import './index.css'
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+    <App />
+);
